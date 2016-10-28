11db13125261d9ce8793bc310d27797eab4de674
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2016-10-27 23:59:50 -0700

    missed a space

--- src/app/App.js
+++ src/app/App.js
@@ -4,7 +4,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import FiltersTable from './BarberDisplay/Filters/FiltersTable'
-// NEED to include schedule data, first and last name?
+// NEED to include schedule data, firstt and last name?
 var BARBERS = [
     {firstname:"Victor", lastname: "Cheng", cuts: ["bold", "buzz", "normal"], ratings: 4.2, profilepicture:"i2.mirror.co.uk/incoming/article5423743.ece/ALTERNATES/s615b/MOST-BEAUTIFUL-FACES.jpg", rate:13, yearscut: 3, description:"I've ben cutting hair since I was born. I love touching people's hair"},
 ];
