6368a49385deb64e99103d29f473ab80bbc107ee
highiqkid <victor_cheng@yahoo.com>
2016-11-02 18:59:33 -0700

    Dashboard.html

--- src/app/Dashboard/Dashboard.js
+++ src/app/Dashboard/Dashboard.js
@@ -3,8 +3,13 @@
  */
 
 class Dashboard extends React.Component{
-    
+    constructor(props){
+        super(props);
+    }
     render(){
-
+        return(
+            <h1> hello </h1>
+        );
     }
 }
+export default Dashboard;
